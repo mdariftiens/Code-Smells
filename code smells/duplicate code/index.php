bd5d14fdb4880dfc4395f8ddc61b4c051322df54
mdariftiens <md.arif.tiens@gmail.com>
2017-10-05 15:04:06 +0600

    long method folder and files added

--- code smells/duplicate code/index.php
+++ code smells/duplicate code/index.php
@@ -1,5 +1,42 @@
-<?php 
+<?php
 
-require 'vendor/autoload.php';
+class Person
+{ 
+	protected $birth_date;
+	
+	public function setBirthdate($date)
+	{
+		$this->birth_date=$date;
+	}
+	
+	public function getBirthdate()
+	{
+		return $this->birth_date;
+	}
+	
+} 
 
+class Customer extends Person
+{
+	public function getAge()
+	{
+		return date('Y') - date('Y', strtotime($this->getBirthdate()));
+	} 
+}
+
+class Vendor extends Person
+{
+	public function getAge()
+	{
+		return date('Y') - date('Y', strtotime($this->getBirthdate()));
+	}
+}
+
+$c=new Customer;
+$c->setBirthdate("2010-12-15");
+echo $c->getAge();
+
+$v=new Vendor;
+$v->setBirthdate("2000-12-15");
+echo $v->getAge();
 ?>
---
+++ code smells/long methode/index.php
@@ -0,0 +1,21 @@
+<?php
+
+class sorting
+{ 
+	private $numbers;
+	
+	public function __construct()
+	{
+		$this->numbers=[5,10,25,7,69,40,35];
+	}
+	
+	
+	public function sell()
+	{
+		// 
+		echo "Before sorting : <br>";
+	}
+} 
+
+
+?>
